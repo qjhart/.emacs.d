92e84193bb60563fcf02739870ba840d4253c7bd
Quinn Hart <qjhart@ucdavis.edu>
2022-01-21 17:44:05 -0800

    Updated to a non-standard ttl file.

--- init.el
+++ init.el
@@ -1,8 +1,9 @@
 ; Configure package.el to include MELPA.
 (require 'package)
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
 ;; Ensure that use-package is installed.
