0f606e11f6e675edb270af37b96171ca15a49541
Quinn Hart <qjhart@ucdavis.edu>
2022-05-06 07:42:34 -0700

    Mostly Updates to basic Emacs Configuration.

--- init.el
+++ init.el
@@ -11,14 +11,16 @@
 ;; If use-package isn't already installed, it's extremely likely that this is a
 ;; fresh installation! So we'll want to update the package repository and
 ;; install use-package before loading the literate configuration.
+
 (when (not (package-installed-p 'use-package))
   (package-refresh-contents)
   (package-install 'use-package))
 
 ;; Load Personal information
 (let ((me "~/.emacs.d/me.org"))
-	(when (file-exists-p me)
-	(org-babel-load-file me)
-	))
+ (when (file-exists-p me)
+   (org-babel-load-file me)
+   )
+ )
 
 (org-babel-load-file "~/.emacs.d/README.org")
