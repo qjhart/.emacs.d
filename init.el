fe46d5747fc8650a261157167dce9d733d107318
Quinn Hart <qjhart@ucdavis.edu>
2024-03-01 08:35:09 -0800

    emacs29 use-package

--- init.el
+++ init.el
@@ -6,16 +6,6 @@
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
 
-;; Ensure that use-package is installed.
-;;
-;; If use-package isn't already installed, it's extremely likely that this is a
-;; fresh installation! So we'll want to update the package repository and
-;; install use-package before loading the literate configuration.
-
-(when (not (package-installed-p 'use-package))
-  (package-refresh-contents)
-  (package-install 'use-package))
-
 ;; Load Personal information
 (let ((me "~/.emacs.d/me.org"))
  (when (file-exists-p me)
